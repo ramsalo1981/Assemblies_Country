a7580b5016e41c78a78b941ac63c84ca3d154f52
ramsalo1981 <rami.salloum@hotmail.com>
2023-10-21 01:16:02 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CAAssemblies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = typeof(Program);
+            var assembly = type.Assembly;
+
+            //var stream = assembly.GetManifestResourceStream("CAAssemblies.data.countries.json");
+            var stream = assembly.GetManifestResourceStream(type, "data.countries.json");
+            var data = new BinaryReader(stream).ReadBytes((int)stream.Length);
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.Write((char)data[i]);
+                System.Threading.Thread.Sleep(300);
+            }
+            stream.Close();
+            Console.ReadKey();
+        }
+    }
+}
